a3c05b272d3f7488653ab916f0b31910cdd936a8
Henry Tao <hi@henrytao.me>
2016-01-16 15:35:51 +0700

    Added gitignore

---
+++ src/main/java/me/henrytao/crackingthecodeinterview/Main.java
@@ -0,0 +1,10 @@
+package me.henrytao.crackingthecodeinterview;
+
+/**
+ * Created by henrytao on 1/16/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello moto");
+    }
+}
