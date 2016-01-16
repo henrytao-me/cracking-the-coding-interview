a200c9bb8402c6b941949c33ce76497e10b4cd58
Henry Tao <hi@henrytao.me>
2016-01-16 16:10:01 +0700

    Added junit

---
+++ src/main/java/me/henrytao/crackingthecodeinterview/question/Q01_01.java
@@ -0,0 +1,11 @@
+package me.henrytao.crackingthecodeinterview.question;
+
+/**
+ * Created by henrytao on 1/16/16.
+ */
+public class Q01_01 {
+
+    public boolean test() {
+        return true;
+    }
+}
---
+++ src/test/java/me/henrytao/crackingthecodeinterview/question/Q01_01_Test.java
@@ -0,0 +1,17 @@
+package me.henrytao.crackingthecodeinterview.question;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by henrytao on 1/16/16.
+ */
+public class Q01_01_Test {
+
+    @Test
+    public void test() throws Exception {
+        assertThat(true, equalTo(true));
+    }
+}
